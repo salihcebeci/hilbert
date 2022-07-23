5d543402e5437b23cdfc08172a2e4b75e97765c0
Salih Cebeci <salihcebeci@gmail.com>
2022-07-23 13:23:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
 
 
+var turtleX;
+var turtleY;
+var turtleHeading = 0;
+var length;
+var W = 800;
+var H = 800;
+var n = 2;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(W, H);
+  turtleX = 0;
+  turtleY = 0;
+  background(0);
+  strokeWeight(30);
+  stroke(255,0,0);
+  noLoop();
 }
 
 function draw() {
-  background(0);
+
+  length = W / n;
+
+  for (var i = 1 ; i < n+1 ; i++){
+    turnRight();
+    oneCurveLeft();
+  }
+}
+
+function oneCurveRight(){
+  walk();
+  turnRight();
+  walk();
+  turnRight();
+  walk();
+}
+
+function oneCurveLeft(){
+  walk();
+  turnLeft();
+  walk();
+  turnLeft();
+  walk();
+}
+
+function walk(){
+  forward(length);
+}
+
+function turnLeft(){
+  rotateTurtle(270);
+}
+
+function turnRight(){
+  rotateTurtle(90);
+}
+
+function forward(amount) {
+  var newX = turtleX + cos(radians(turtleHeading)) * amount;
+  var newY = turtleY + sin(radians(turtleHeading)) * amount;
+  line(turtleX, turtleY, newX, newY);
+  fill(0);
+  turtleX = newX;
+  turtleY = newY;
+}
+
+function rotateTurtle(degrees) {
+  turtleHeading += degrees;
 }
